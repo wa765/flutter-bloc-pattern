8b74e5de68e63bed1dab6ed0c60f097d37767da8
wa765 <wahyusaputra12345678@gmail.com>
2022-05-11 11:44:47 +0700

    error button

--- lib/counter_bloc.dart
+++ lib/counter_bloc.dart
@@ -1,7 +1,47 @@
 import 'dart:async';
 
-class CounterBloc {
-  final stateStreamController = StreamController<int>();
+enum CounterAction { Increment, Decrement, Reset }
 
-  // StreamSink<int> get counterSink
+class CounterBloc {
+  late int counter;
+
+  final _stateStreamController = StreamController<int>();
+  // sink for input
+  StreamSink<int> get counterSink {
+    return _stateStreamController.sink;
+  }
+
+  // stream for output
+  Stream<int> get counterStream {
+    return _stateStreamController.stream;
+  }
+
+  //
+  final _eventStreamController = StreamController<CounterAction>();
+  // sink for input
+  StreamSink<CounterAction> get eventSink {
+    return _eventStreamController.sink;
+  }
+
+  // stream for output
+  Stream<CounterAction> get eventStream {
+    return _eventStreamController.stream;
+  }
+
+  // Logic Event
+  CounterBloc() {
+    counter = 0;
+    eventStream.listen((event) {
+      if (event == CounterAction.Increment) {
+        counter++;
+      } else if (event == CounterAction.Decrement) {
+        counter--;
+      } else if (event == CounterAction.Reset) {
+        counter = 0;
+
+        //
+        counterSink.add(counter);
+      }
+    });
+  }
 }
--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 
+import 'counter_bloc.dart';
+
 void main() {
   runApp(const MyApp());
 }
@@ -48,18 +50,7 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 10;
-
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter -= 2;
-    });
-  }
+  final counterBloc = CounterBloc();
 
   @override
   Widget build(BuildContext context) {
@@ -95,18 +86,23 @@ class _MyHomePageState extends State<MyHomePage> {
           // horizontal).
           mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headline4,
+            Text("halo"),
+            StreamBuilder(
+              stream: counterBloc.counterStream,
+              builder: (context, snapshot) {
+                return Text(
+                  '${snapshot.data}',
+                  style: Theme.of(context).textTheme.headline4,
+                );
+              },
             ),
           ],
         ),
       ),
       floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
+        onPressed: () {
+          counterBloc.eventSink.add(CounterAction.Increment);
+        },
         tooltip: 'Increment',
         child: const Icon(Icons.add),
       ), // This trailing comma makes auto-formatting nicer for build methods.
